63c23bccb4f77934550fdd04317778ac6f225b91
LeeMyungHak <mh9716@naver.com>
2020-07-01 19:19:28 +0000

    edited bs 512 epoch 100

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
-python3 trainer.py --output_dir ./rsc/output_samplecnn \
+python3 trainer.py --output_dir ./rsc/output_samplecnn_4 \
         --train_feature ./rsc/train.hdf5 \
-        --train_batch_size 256 \
+	--num_train_epochs 100.0 \
+        --train_batch_size 512 \
       	--num_workers 16
